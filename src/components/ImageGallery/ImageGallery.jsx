adc73686577ebd26f0e9cf204445ca4dd77491c8
Yevhen Chementsov <yevhenchementsov@gmail.com>
2023-06-04 20:30:35 +0300

    Add react-toastify package

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -6,25 +6,31 @@ export default class ImageGallery extends Component {
   state = {
     images: [],
     error: null,
+    currentPage: 1,
+    totalImages: null,
   };
 
   async componentDidUpdate(prevProps, prevState) {
     if (prevProps.value !== this.props.value) {
       try {
-        const data = await fetchImages(this.props.value);
-        console.log(data);
-      } catch (error) {}
-      //   .then(({ hits }) =>
-      //     this.setState(({ images }) => ({
-      //       images: [...images, ...hits],
-      //     })),
-      //   );
+        const { hits, totalHits } = await fetchImages(
+          this.props.value,
+          this.state.currentPage,
+        );
+
+        this.setState(({ images }) => ({
+          images: [...images, ...hits],
+        }));
+      } catch (error) {
+        this.setState({ error });
+      }
     }
   }
 
   render() {
     return (
       <ul>
+        {this.state.error && <h1>{this.state.error.message}</h1>}
         {this.state.images.map(image => (
           <li key={image.id}>
             <img src={image.webformatURL} alt={image.tags} />
