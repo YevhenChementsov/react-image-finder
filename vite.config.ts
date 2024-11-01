b101c3db81ef774dd760e0552a4f505a81c26332
Yevhen Chementsov <yevhenchementsov@gmail.com>
2024-11-01 12:42:14 +0200

    Add dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+import { resolve } from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/react-image-finder/' : '/',
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      components: resolve(__dirname, 'src/components'),
+      constants: resolve(__dirname, 'src/constants'),
+    },
+  },
+});
